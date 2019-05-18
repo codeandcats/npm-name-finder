5c267e596bcb714ed89335109159cec8df6c81a6
Ben <codeandcats@gmail.com>
2019-05-18 16:41:07 +1000

    Updates

---
+++ precache-manifest.9d4c4ffb14e2bd392fe602181e92cf8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896f8dea150e948183dd04b410817250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d57d9ea5a1a00a3b1cb7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "099a8783cbd3e39cba49",
+    "url": "/static/js/2.07e5fc1e.chunk.js"
+  },
+  {
+    "revision": "d57d9ea5a1a00a3b1cb7",
+    "url": "/static/js/main.4387d8e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
